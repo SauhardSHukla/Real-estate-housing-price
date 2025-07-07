675628ba1d65954328373ccd038fc013b5dfe67c
SauhardSHukla <sauhardshukla03@gmail.com>
2025-07-07 23:27:44 +0530

    New folder

--- JypterNoteBook/Sampling_Techniques/Outliers_function.py
+++ JypterNoteBook/Sampling_Techniques/Outliers_function.py
@@ -1,19 +1,17 @@
-def detect_outlier_zscore(df,col_name,threshold=3):
-  df_copy = df.copy()
-  mean = df_copy[col_name].mean()
-  std = df_copy[col_name].std()
-  z_score=(df[col_name] - mean)/std
-  df[f'Outliers_{col_name}'] = z_score.abs()>threshold
-  return df
+def tag_outliers(df, col, method='zscore', threshold=3):
+    if method == 'zscore':
+        z_scores = (df[col] - df[col].mean()) / df[col].std()
+        mask = (z_scores > -threshold) & (z_scores < threshold)
+        filtered_df = df[mask]
 
-def detect_outlier_IOR(df,col_names):
-  df_copy =df.copy()
-  Q1=  df[col_names].quantile(0.25)
-  Q3=  df[col_names].quantile(0.75)
-  IQR = Q3-Q1
-  Lower = Q1-1.5*IQR
-  Upper = Q3+1.5*IQR
-  df[f'Outliers_{col_names}'] =~df[col_names].between(Lower,Upper)
-  return df
-  
+    elif method == 'iqr':
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        mask = (df[col] >= lower_bound) & (df[col] <= upper_bound)
+        filtered_df = df[mask]
 
+    print(f"{col}: Removed {len(df) - len(filtered_df)} outliers")
+    return filtered_df
