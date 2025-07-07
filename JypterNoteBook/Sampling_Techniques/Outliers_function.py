2be34a7d09f58149ad54b3f0cb9a40ca6ab79f57
SauhardSHukla <sauhardshukla03@gmail.com>
2025-07-07 19:41:14 +0530

    New folder

--- JypterNoteBook/Sampling_Techniques/Outliers_function.py
+++ JypterNoteBook/Sampling_Techniques/Outliers_function.py
@@ -1,2 +1,19 @@
-def tag_outliers(df,threshold):
+def detect_outlier_zscore(df,col_name,threshold=3):
+  df_copy = df.copy()
+  mean = df_copy[col_name].mean()
+  std = df_copy[col_name].std()
+  z_score=(df[col_name] - mean)/std
+  df[f'Outliers_{col_name}'] = z_score.abs()>threshold
+  return df
+
+def detect_outlier_IOR(df,col_names):
+  df_copy =df.copy()
+  Q1=  df[col_names].quantile(0.25)
+  Q3=  df[col_names].quantile(0.75)
+  IQR = Q3-Q1
+  Lower = Q1-1.5*IQR
+  Upper = Q3+1.5*IQR
+  df[f'Outliers_{col_names}'] =~df[col_names].between(Lower,Upper)
+  return df
   
+
